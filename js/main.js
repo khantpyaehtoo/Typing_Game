48da82017fa5e7e9f9464ff2992e644a228c98fd
khantpyaehtoo <ericrebillet@gmail.com>
2026-02-28 22:23:07 +0630

    first commit

--- js/main.js
+++ js/main.js
@@ -1,20 +1,70 @@
 const textDisplay = document.getElementById("text-display");
 const userInput = document.getElementById("user-input");
 
-const hard = "http://localhost:3001/hard";
+let diffBtns = document.querySelectorAll(".diff-btns");
+diffBtns.forEach((diffBtn) => {
+    diffBtn.addEventListener("click", (e) => {
+        e.preventDefault();
 
-const text = "The archaeological expedition unearthed artifacts that";
-text.split("").forEach((char) => {
-    const span = document.createElement("span");
-    span.innerText = char;
-    textDisplay.appendChild(span);
+        diffBtns.forEach((b) => b.classList.remove("clicked"));
+        diffBtn.classList.add("clicked");
+
+        let currLvl = diffBtn.innerText.toLowerCase();
+        initGame(currLvl);
+        resetStats();
+    });
 });
+
+let modeBtns = document.querySelectorAll(".mode-btn");
+modeBtns.forEach((modeBtn) => {
+    modeBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        modeBtns.forEach((b) => b.classList.remove("clicked"));
+        modeBtn.classList.add("clicked");
+    });
+});
+
+const fetchApi = async (lvl) => {
+    try {
+        const res = await fetch(`http://localhost:3001/${lvl}`);
+        const data = await res.json();
+
+        const randomIndex = Math.floor(Math.random() * data.length);
+        const randomQuote = data[randomIndex];
+
+        console.log(randomQuote);
+        return randomQuote.text;
+    } catch (error) {
+        console.error("error at fetching api", error);
+    }
+};
+
+const initGame = async (d = "medium") => {
+    const data = await fetchApi(d);
+    if (data) {
+        textDisplay.innerHTML = "";
+        data.split("").forEach((char) => {
+            const span = document.createElement("span");
+            span.innerText = char;
+            textDisplay.appendChild(span);
+        });
+    }
+};
+
+initGame();
 
 document.addEventListener("click", () => userInput.focus());
 
+let totalTyped = 0;
+let correctTyped = 0;
 userInput.addEventListener("input", () => {
+    // e.preventDefault();
     const quote = textDisplay.querySelectorAll("span");
     const value = userInput.value.split("");
+
+    let currentCorrect = 0;
+    let currentTotal = value.length;
 
     quote.forEach((charSpan, index) => {
         const char = value[index];
@@ -22,11 +72,25 @@ userInput.addEventListener("input", () => {
             charSpan.classList.remove("correct");
             charSpan.classList.remove("incorrect");
         } else if (char === charSpan.innerText) {
-            charSpan.classList.remove("incorrect");
             charSpan.classList.add("correct");
+            charSpan.classList.remove("incorrect");
+
+            currentCorrect++;
         } else {
             charSpan.classList.add("incorrect");
             charSpan.classList.remove("correct");
         }
     });
+
+    // Accuracy
+    if (currentTotal > 0) {
+        const accuracy = Math.floor((currentCorrect / currentTotal) * 100);
+        document.getElementById("display-accuracy").innerText = `${accuracy}`;
+    } else {
+        document.getElementById("display-accuracy").innerText = "100";
+    }
 });
+
+function resetStats() {
+    document.getElementById("display-accuracy").innerText = "100";
+}
